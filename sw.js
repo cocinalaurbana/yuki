36ab8898ff29e5ba3d4a474c7f22c89d2aab245a
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-06 16:51:13 +0200

    Actualiza caché de recetas vegetarianas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v9';
+const CACHE = 'yuki-v10';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
@@ -18,6 +18,7 @@ self.addEventListener('fetch', e => {
     caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
+
 
 
 
