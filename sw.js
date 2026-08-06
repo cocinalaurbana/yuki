cc8bfc9f7e60b6ce3c0363ff7bdd0907b1b7b709
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-06 18:55:28 +0200

    Actualiza caché del Inicio y el icono

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v19';
+const CACHE = 'yuki-v20';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
@@ -18,6 +18,7 @@ self.addEventListener('fetch', e => {
     caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
+
 
 
 
