84abe1f60b1489b5f0200c6e62892400bf22de5a
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-08 19:27:50 +0200

    Actualiza caché de checks de prácticas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v37';
+const CACHE = 'yuki-v38';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
