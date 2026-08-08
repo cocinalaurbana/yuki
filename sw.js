d81ca28c6ec91fe763cfa5dd06f88a4b14b40fcf
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-08 18:55:14 +0200

    Actualiza caché de etiquetas de enlaces

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v34';
+const CACHE = 'yuki-v35';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
