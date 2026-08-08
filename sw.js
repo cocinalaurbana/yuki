ed749229e651d1a3a731a98ff8e246cf994e0cc8
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-08 10:16:32 +0200

    Actualiza caché para apertura en Firefox

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v25';
+const CACHE = 'yuki-v26';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
