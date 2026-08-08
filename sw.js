450bd9ba4df072d2cebc44649fc005789bdbfa26
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-08 18:57:19 +0200

    Actualiza caché del selector de práctica

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v35';
+const CACHE = 'yuki-v36';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
