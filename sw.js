ed953145159becfbfd14f478cd73e538994c0075
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-08 17:50:43 +0200

    Actualiza caché del orden definitivo de Salud

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v30';
+const CACHE = 'yuki-v31';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
