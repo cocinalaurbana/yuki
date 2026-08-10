8b6ad353728f92c453a4390f3220bdc93cc00945
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-10 17:14:16 +0200

    Mejora la vista horizontal y vertical de Yuki

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v38';
+const CACHE = 'yuki-v39';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
