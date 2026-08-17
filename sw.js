bfe5c0d88e5100de755421fa9ca20872aa298d48
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-17 12:05:28 +0200

    Corrige el acceso instalado en Yuki

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'yuki-v39';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const CACHE = 'yuki-v40';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
