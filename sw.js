f1da3cfbfe2c8bdd9cf32c16aa36d4ccd1bf8134
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-25 14:23:25 +0200

    Actualiza la caché para cargar la tipografía nueva

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v40';
+const CACHE = 'yuki-v42';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
