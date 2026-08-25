e3941c74f9ef3256e4a3d97416f748ff6e32afca
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-25 17:10:11 +0200

    Renueva la caché del acceso a vídeos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v54';
+const CACHE = 'yuki-v55';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
