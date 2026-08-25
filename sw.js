d62b80d0642fa98b028908d8e48102e53738add7
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-25 15:20:48 +0200

    Renueva la caché de la paginación

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v46';
+const CACHE = 'yuki-v47';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
