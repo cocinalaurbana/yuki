43341eb948bd0284b098e7193eff4b3b80b67075
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-25 16:13:21 +0200

    Renueva la caché de la cuadrícula de práctica

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v52';
+const CACHE = 'yuki-v53';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
