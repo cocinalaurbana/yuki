b7aa925b78e1319870a99dddbd23d98fd3aa6327
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-25 14:25:21 +0200

    Renueva la caché tras ampliar los botones

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v42';
+const CACHE = 'yuki-v43';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
