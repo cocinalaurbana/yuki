334858b9e0f88c3a26590392ea9c340d37b6b716
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-25 14:30:07 +0200

    Renueva la caché del modo libro

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v43';
+const CACHE = 'yuki-v45';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
