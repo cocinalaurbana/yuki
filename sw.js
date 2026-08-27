176c80b636c7aa0cf185b2e9dfe8da6da74793ba
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-27 21:58:34 +0200

    Renueva la caché de las recetas personales

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v56';
+const CACHE = 'yuki-v57';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
