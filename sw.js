830922dca4be238d7c0d695195579f8e1bbc8c86
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-28 10:37:59 +0200

    Actualiza la caché para la navegación por páginas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'yuki-v58';
+const CACHE = 'yuki-v59';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
