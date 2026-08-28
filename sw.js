a37677664baa87b47a4cec8fbe2317b28a552960
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-28 18:04:06 +0200

    Actualiza la caché del botón Yuki qué hago

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'yuki-v65';
-const ASSETS = ['./', './?v=65', './index.html?v=65', './manifest.json', './manifest-v65.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'yuki-v66';
+const ASSETS = ['./', './?v=66', './index.html?v=66', './manifest.json', './manifest-v66.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
