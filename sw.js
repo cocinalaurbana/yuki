2a7053f285a9c5e1cc86b6a3b71ba2bf629746ea
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-29 09:25:11 +0200

    Actualiza caché de Yuki v81

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'yuki-v78';
-const ASSETS = ['./', './?v=78', './index.html?v=78', './manifest.json', './manifest-v78.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'yuki-v81';
+const ASSETS = ['./', './?v=81', './index.html?v=81', './manifest.json', './manifest-v81.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
