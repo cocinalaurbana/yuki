8a0c795e0ce9df373cf2544bf94b40b972f25ef0
cocinalaurbana <cocinalaurbana@gmail.com>
2026-08-31 02:44:41 -0500

    Publica Yuki v91 · reorganiza Inicio
    Reorganiza la pantalla de Inicio alrededor de Cómo llegas, tiempo disponible y una sola propuesta. Mantiene todas las funciones, actualiza la ayuda y renueva la caché de la PWA.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'yuki-v90';
-const ASSETS = ['./', './?v=90', './index.html?v=90', './manifest.json', './manifest-v90.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'yuki-v91';
+const ASSETS = ['./', './?v=91', './index.html?v=91', './manifest.json', './manifest-v91.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
